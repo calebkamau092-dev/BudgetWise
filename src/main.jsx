78261405c5c3190028d11aa0f5e7c74e0e11dda7
MartinRerei <martin.rerei@student.moringaschool.com>
2026-08-12 15:36:52 +0300

    Make full BudgetWise dashboard the main application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AddTransaction from './Components/AddTransaction'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AddTransaction />
+    <App />
   </StrictMode>,
 )
